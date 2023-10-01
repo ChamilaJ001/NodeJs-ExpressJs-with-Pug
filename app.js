3053905854f6c6dad0923f364b2fef5ff4f2acc8
ChamilaJ001 <official.chamilaj@gmail.com>
2023-10-01 12:43:46 +0530

    new

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminData = require("./Routes/admin");
+const shopRoutes = require("./Routes/shop");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
